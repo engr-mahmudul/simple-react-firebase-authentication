2a7e3cb33ea0ad68916898908ddc0e9473bf2e56
Mahmudul Hasan <17101120@uap-bd.edu>
2021-10-09 14:22:43 +0600

    Github provider added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, GithubAuthProvider , getAuth, signInWithPopup } from "firebase/auth";
 import logo from './logo.svg';
 import './App.css';
 import initializeAuthentication from './Firebase/firebase.initialization';
@@ -6,39 +6,55 @@ import { useState } from "react";
 // Authentication Initilize
 initializeAuthentication()
 //Log in procedure start
-const provider = new GoogleAuthProvider();
-
+const googelProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
 
 function App() {
-  const [user,setUser] = useState({})
-  const signInHandler = () => {
+  const [user, setUser] = useState({})
+  const signInGoogleHandler = () => {
     const auth = getAuth();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const { displayName, photoURL ,email} = result.user;
-        const logedInUsers ={
-          name:displayName,
-          photo:photoURL,
+    signInWithPopup(auth, googelProvider)
+      .then(result => {
+        const { displayName, photoURL, email } = result.user;
+        const logedInUsers = {
+          name: displayName,
+          photo: photoURL,
           email: email
         }
         setUser(logedInUsers)
 
       })
   }
+  const signInGithubHandler=()=>{
+    const auth = getAuth();
+  signInWithPopup(auth, githubProvider)
+  .then(result =>{
+    const {displayName,photoURL} = result.user;
+    const logedInUsers = {
+      name:displayName,
+      photo:photoURL
+    }
+    setUser(logedInUsers);
+    
+  })
+  }
   return (
     <div className='custom'>
       <div>
-        <button onClick={signInHandler}>Button click</button>
+        <button onClick={signInGoogleHandler}>Google SignIn</button>
+        <button onClick={signInGithubHandler}>Github SignIn</button>
       </div>
       <div>
 
         {
-          
-            user.email && <div> 
+
+          user.name && <div>
+            <div className='smalldiv'>
               <img src={user.photo} alt="" />
-              <h2>{user.name}</h2>
-              <h4>{user.email}</h4>
             </div>
+            <h2>{user.name}</h2>
+            <h4>{user.email}</h4>
+          </div>
 
         }
 
