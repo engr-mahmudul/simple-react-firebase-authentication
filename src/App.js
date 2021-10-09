ef8527291f19d31d280b314479bd9d8fdcadfefc
Mahmudul Hasan <17101120@uap-bd.edu>
2021-10-09 11:08:20 +0600

    show user data in UI

--- src/App.js
+++ src/App.js
@@ -2,23 +2,47 @@ import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import logo from './logo.svg';
 import './App.css';
 import initializeAuthentication from './Firebase/firebase.initialization';
+import { useState } from "react";
 // Authentication Initilize
 initializeAuthentication()
 //Log in procedure start
 const provider = new GoogleAuthProvider();
-const signInHandler = () => {
-  const auth = getAuth();
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      const user = result.user;
-      console.log(user)
-    })
-}
+
 
 function App() {
+  const [user,setUser] = useState({})
+  const signInHandler = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const { displayName, photoURL ,email} = result.user;
+        const logedInUsers ={
+          name:displayName,
+          photo:photoURL,
+          email: email
+        }
+        setUser(logedInUsers)
+
+      })
+  }
   return (
-    <div className="App">
-      <button onClick={signInHandler}>Button click</button>
+    <div className='custom'>
+      <div>
+        <button onClick={signInHandler}>Button click</button>
+      </div>
+      <div>
+
+        {
+          
+            user.email && <div> 
+              <img src={user.photo} alt="" />
+              <h2>{user.name}</h2>
+              <h4>{user.email}</h4>
+            </div>
+
+        }
+
+      </div>
     </div>
   );
 }
